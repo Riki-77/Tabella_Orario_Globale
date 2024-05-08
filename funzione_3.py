d3095dc5bc176a7f56a2284c2b516ad044666447
Riki-77 <135115582+Riki-77@users.noreply.github.com>
2024-05-08 12:25:52 +0200

    funzione_3.py

--- funzione_3.py
+++ funzione_3.py
@@ -1,11 +1,15 @@
-def cognome_nome(nome):
-    file=open('OrarioTabellaGlobale.csv', 'r')
-    filecsv=csv.reader(file) #apre il file csv
-    for row in filecsv: #cerca quante volte "D" è presente nella riga del docente inserito
-        if nome in row[0]:
-            contatore=0
-            for ora in row:
-                if ' D' in ora: #spazio prima di D per non prendere D sbagliate come ad esempio le classi
-                    contatore+=1
-            break
-    return contatore
+def Ore_Disponibili(docenti,nomedocente):
+    orario=[]
+    
+    for i in range(len(docenti)):
+        if nomedocente in docenti[i]:
+            for j in range(1,len(docenti[i])):
+                orario.append(docenti[i][j])
+    
+    cont=0
+    
+    for i in range(len(orario)):
+        if orario[i] == " D ":
+            cont+=1
+    
+    return (cont)
