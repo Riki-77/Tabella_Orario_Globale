3e67578146041b8f20e1de5a5bd7eebe4aa2432e
Riki-77 <135115582+Riki-77@users.noreply.github.com>
2024-05-14 22:06:24 +0200

    funzione_2.py

--- funzione_2.py
+++ funzione_2.py
@@ -1,4 +1,26 @@
-def docente(doc):
-    file=open("OrarioTabellaGlobale.csv", "r")
-    filecsv=csv.reader(file) #apre il file csv
-    return
+def orario_docente(docente):
+    file = open('OrarioTabellaGlobale.csv','r')
+    campi = next(file)
+    ore = next(file)
+    count = 0
+    for row in file:
+        if docente in row:
+            riga = row.split(",")
+            riga.pop(0)
+            for elem in riga:
+                if elem !="   ":
+                    count += 1
+        continue
+    file.close()
+    return campi, ore, riga, count
+
+#main
+docente =input('Inserire il docente: ')
+orario = orario_docente(docente)
+for parte in orario:
+        if parte is orario[3]:
+            continue
+        else:
+            print(parte)
+       
+print(f'ORE TOTALE DI {docente} SONO {orario[3]}')
